6b44b6824963252a926c20a1363efa580cc13508
Vio0330 <vio0330@naver.com>
2024-02-26 20:04:47 +0900

    korea

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-
+import './assets/css/korea-map-font-v1.css';
 
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div>
+      <ul className="korea-map-font-v1">
+        <li className="강원">a</li>
+        <li className="경기">b</li>
+        <li className="경남">c</li>
+        <li className="경북">d</li>
+        <li className="광주">e</li>
+        <li className="대구">f</li>
+        <li className="대전">g</li>
+        <li className="부산">h</li>
+        <li className="서울">i</li>
+        <li className="세종">j</li>
+        <li className="울산">k</li>
+        <li className="인천">l</li>
+        <li className="전남">m</li>
+        <li className="전북">n</li>
+        <li className="제주">o</li>
+        <li className="충남">p</li>
+        <li className="충북">q</li>
+      </ul>
+    </div>
   )
 }
 
